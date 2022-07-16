574f960445a9c493e788c81826a4720de2020958
junzhou1999 <outtatouch2020.com>
2022-07-16 22:27:45 +0800

    3-3 增加通用返回类CommonResp

--- src/main/java/org/abc/wiki/controller/EbookController.java
+++ src/main/java/org/abc/wiki/controller/EbookController.java
@@ -1,6 +1,7 @@
 package org.abc.wiki.controller;
 
 import org.abc.wiki.domain.Ebook;
+import org.abc.wiki.resp.CommonResp;
 import org.abc.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,8 +18,11 @@ public class EbookController {
 	private EbookService ebookService;
 
 	@RequestMapping(value = "/list", method = RequestMethod.GET)
-	public List<Ebook> list() {
-		return ebookService.list();
+	public CommonResp<List<Ebook>> list() {
+		List<Ebook> list = ebookService.list();
+		CommonResp<List<Ebook>> resp = new CommonResp<>();
+		resp.setContent(list);
+		return resp;
 	}
 
 }
