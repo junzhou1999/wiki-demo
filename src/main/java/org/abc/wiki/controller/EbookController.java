a355d84bcf7961152b5d9a0fd866d8d59dfc20f2
junzhou1999 <outtatouch2020.com>
2022-07-16 23:36:56 +0800

    3-4 封装请求参数和返回参数

--- src/main/java/org/abc/wiki/controller/EbookController.java
+++ src/main/java/org/abc/wiki/controller/EbookController.java
@@ -1,7 +1,8 @@
 package org.abc.wiki.controller;
 
-import org.abc.wiki.domain.Ebook;
+import org.abc.wiki.req.EbookReq;
 import org.abc.wiki.resp.CommonResp;
+import org.abc.wiki.resp.EbookResp;
 import org.abc.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,12 +18,25 @@ public class EbookController {
 	@Resource
 	private EbookService ebookService;
 
+/*
+	// 前端可以直接传递name参数进来
 	@RequestMapping(value = "/list", method = RequestMethod.GET)
-	public CommonResp<List<Ebook>> list() {
-		List<Ebook> list = ebookService.list();
+	public CommonResp<List<Ebook>> list(String name) {
+		List<Ebook> list = ebookService.list(name);
 		CommonResp<List<Ebook>> resp = new CommonResp<>();
 		resp.setContent(list);
 		return resp;
 	}
+*/
 
+	/**
+	 * @param ebookReq 只要类里边的名字跟前端传进来的参数匹配，Spring会自动映射
+	 */
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	public CommonResp<List<EbookResp>> list(EbookReq ebookReq) {
+		List<EbookResp> list = ebookService.list(ebookReq);
+		CommonResp<List<EbookResp>> resp = new CommonResp<>();
+		resp.setContent(list);
+		return resp;
+	}
 }
--- src/main/java/org/abc/wiki/service/EbookService.java
+++ src/main/java/org/abc/wiki/service/EbookService.java
@@ -1,10 +1,15 @@
 package org.abc.wiki.service;
 
 import org.abc.wiki.domain.Ebook;
+import org.abc.wiki.domain.EbookExample;
 import org.abc.wiki.mapper.EbookMapper;
+import org.abc.wiki.req.EbookReq;
+import org.abc.wiki.resp.EbookResp;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -13,7 +18,35 @@ public class EbookService {
 	@Resource
 	private EbookMapper ebookMapper;
 
-	public List<Ebook> list() {
-		return ebookMapper.selectByExample(null);
+	/*
+	public List<Ebook> list(String name) {
+		EbookExample ebookExample = new EbookExample();
+		// where条件类
+		EbookExample.Criteria criteria = ebookExample.createCriteria();
+
+		criteria.andNameLike("%" + name + "%");
+		return ebookMapper.selectByExample(ebookExample);
+	}
+	*/
+
+	public List<EbookResp> list(EbookReq ebookReq) {
+		EbookExample ebookExample = new EbookExample();
+		// where条件类
+		EbookExample.Criteria criteria = ebookExample.createCriteria();
+
+		// 模糊查询条件
+		criteria.andNameLike("%" + ebookReq.getName() + "%");
+
+		ArrayList<EbookResp> respList = new ArrayList<>();
+		// Ebook信息转换成EbookResp
+		List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+		for (Ebook ebook : ebookList) {
+			EbookResp ebookResp = new EbookResp();
+			// 使用Spring的工具可以把属性直接copy到另一个实体类
+			BeanUtils.copyProperties(ebook, ebookResp);
+			respList.add(ebookResp);
+		}
+
+		return respList;
 	}
 }
