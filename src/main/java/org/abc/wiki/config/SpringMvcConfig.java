d933a3cc38ad02ffa3fd03022f5cf65eccd930c9
junzhou1999 <outtatouch2020.com>
2022-07-21 13:31:30 +0800

    6-2 Spring增加拦截器，打印接口耗时

---
+++ src/main/java/org/abc/wiki/config/SpringMvcConfig.java
@@ -0,0 +1,21 @@
+package org.abc.wiki.config;
+
+import org.abc.wiki.interceptor.LogInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+	@Resource
+	private LogInterceptor logInterceptor;
+
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(logInterceptor)
+				.addPathPatterns("/**")  // 拦截所有接口
+				.excludePathPatterns("/login");  // 不拦截登录也页面
+	}
+}
