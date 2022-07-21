f0cc6fbf4e9ae8a0abc0ba6e59e8b2b4ade6c399
junzhou1999 <outtatouch2020.com>
2022-07-21 18:59:54 +0800

    7-3 修改电子书管理页面布局

--- web/src/views/admin/AdminEbook.vue
+++ web/src/views/admin/AdminEbook.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>
-    <h1>电子书管理页面</h1>
-  </div>
+    <a-layout>
+        <a-layout-content
+                :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+        >
+            <div>
+                <h1>电子书管理页面</h1>
+            </div>
+        </a-layout-content>
+    </a-layout>
 </template>
