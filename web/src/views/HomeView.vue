b595c8520cee6b8dbb18addb9d100257b54ac456
junzhou1999 <outtatouch2020.com>
2022-07-28 22:05:48 +0800

    8-8 首页显示分类菜单

--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -1,45 +1,34 @@
 <template>
     <a-layout>
         <a-layout-sider width="200" style="background: #fff">
-            <a-menu
+            <a-menu :style="{ height: '100%', borderRight: 0 }"
                     mode="inline"
-                    :style="{ height: '100%', borderRight: 0 }"
             >
-                <a-sub-menu key="sub1">
-                    <template #title>
-              <span>
-                <user-outlined/>
-                subnav 111
-              </span>
+                <a-menu-item>
+                    <template #icon>
+                        <MailOutlined/>
                     </template>
-                    <a-menu-item key="1">option1</a-menu-item>
-                    <a-menu-item key="2">option2</a-menu-item>
-                    <a-menu-item key="3">option3</a-menu-item>
-                    <a-menu-item key="4">option4</a-menu-item>
-                </a-sub-menu>
-                <a-sub-menu key="sub2">
-                    <template #title>
-              <span>
-                <laptop-outlined/>
-                subnav 2
-              </span>
+                    欢迎
+                </a-menu-item>
+
+                <!-- 有子菜单的用a-sub-menu标签 -->
+                <!-- 最深3层的菜单 -->
+                <a-sub-menu v-for="item in level1" :key="item.id" :title="item.name">
+
+                    <template #icon>
+                        <UserAddOutlined/>
                     </template>
-                    <a-menu-item key="5">option5</a-menu-item>
-                    <a-menu-item key="6">option6</a-menu-item>
-                    <a-menu-item key="7">option7</a-menu-item>
-                    <a-menu-item key="8">option8</a-menu-item>
-                </a-sub-menu>
-                <a-sub-menu key="sub3">
-                    <template #title>
-              <span>
-                <notification-outlined/>
-                subnav 3
-              </span>
+
+                    <template v-for="child in item.children" :key="child.id">
+                        <a-menu-item v-if="child.children == null" :key="child.id">
+                            <span>{{child.name}}</span>
+                        </a-menu-item>
+                        <a-sub-menu v-else :key="child.id" :title="child.name">
+                            <a-menu-item v-for="child1 in child.children" :key="child1.id">
+                                <span>{{child1.name}}</span>
+                            </a-menu-item>
+                        </a-sub-menu>
                     </template>
-                    <a-menu-item key="9">option9</a-menu-item>
-                    <a-menu-item key="10">option10</a-menu-item>
-                    <a-menu-item key="11">option11</a-menu-item>
-                    <a-menu-item key="12">option12</a-menu-item>
                 </a-sub-menu>
             </a-menu>
         </a-layout-sider>
@@ -74,18 +63,23 @@
 <script lang="ts">
     import {defineComponent, onMounted, ref} from 'vue';
     import axios from 'axios';
-    import {StarOutlined, LikeOutlined, MessageOutlined} from '@ant-design/icons-vue';
+    import {StarOutlined, LikeOutlined, MessageOutlined, MailOutlined, UserAddOutlined} from '@ant-design/icons-vue';
+    import {Tool} from "@/util/tool";
+    import {message} from "ant-design-vue";
 
     export default defineComponent({
         name: 'HomeView',
         StarOutlined,
         LikeOutlined,
         MessageOutlined,
+        MailOutlined,
+        UserAddOutlined,
 
         setup() {
             const ebooks = ref();  //1
 
             onMounted(() => {
+                handleQueryCategory();
                 axios.get('/ebook/list', {
                     params: {
                         page: 1,
@@ -104,9 +98,26 @@
                 {type: 'MessageOutlined', text: '2'},
             ];
 
+            const level1 = ref();
+            const handleQueryCategory = () => {
+                axios.get("/category/all").then((response) => {
+                    const data = response.data;
+                    if (data.success) {
+                        const categorys = data.content;
+                        level1.value = [];
+                        level1.value = Tool.array2Tree(categorys, 0);
+                        console.log("aaa " + JSON.stringify(level1.value));
+                    } else {
+                        message.error(data.message)
+                    }
+                });
+            };
+
             return {
                 ebooks,  //3
                 actions,
+
+                level1
             }
         }  // setup
     });
