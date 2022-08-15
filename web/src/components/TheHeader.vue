8e4559d4b3d01770a9937d4ee4d369036cb20bf7
junzhou1999 <outtatouch2020.com>
2022-08-15 17:50:13 +0800

    10-21 增加自定义登录表单验证

--- web/src/components/TheHeader.vue
+++ web/src/components/TheHeader.vue
@@ -44,14 +44,16 @@
             v-model:visible="loginModalVisible"
             title="登录表单"
             :confirm-loading="loginModalLoading"
+            ok-text="登录"
+            cancel-text="取消"
+            :okButtonProps="{disabled: (loginUser.loginName === '' || vPassword4Ok() === false)}"
             @ok="handleLogin"
     >
-        <a-form :model="loginUser" :label-col="{ span:4 }" :wrapper-col="{ span:22 } ">
-            <a-form-item label="用户名" name="loginName" :rules="[{ required: true, message: '请输入用户名！' }]">
-                <!-- 前端编辑不允许修改登录 -->
+        <a-form :model="loginUser" :label-col="{ span:4 }" :wrapper-col="{ span:22 }" :rules="rules">
+            <a-form-item label="用户名" name="loginName" has-feedback>
                 <a-input v-model:value="loginUser.loginName"/>
             </a-form-item>
-            <a-form-item label="密码" name="password" :rules="[{ required: true, message: '请输入密码！' }]">
+            <a-form-item label="密码" name="password" has-feedback>
                 <a-input-password v-model:value="loginUser.password"/>
             </a-form-item>
         </a-form>
@@ -65,6 +67,7 @@
     import {message} from "ant-design-vue";
     import {Tool} from "@/util/tool";
     import store from "@/store";
+    import type {Rule} from 'ant-design-vue/es/form';
 
     // js定义变量是存在的
     declare let hexMd5: any;
@@ -88,32 +91,55 @@
                     return store.state.user;
                 });
 
+            // 表单验证
+            const vLoginName = async (_rule: Rule, value: string) => {
+                if (value === '') {
+                    return Promise.reject('请输入用户名！');
+                }
+            };
+            const vPassword = async (_rule: Rule, value: string) => {
+                if (value === '') {
+                    return Promise.reject('请输入密码！');
+                }
+                const regexpDef = /^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,16}$/;
+                if (!value.match(regexpDef)) {
+                    return Promise.reject('【密码】至少包含数字和英文，长度6-16');
+                }
+            };
+            const rules: Record<string, Rule[]> = {
+                loginName: [{required: true, validator: vLoginName, trigger: 'change'}],
+                password: [{required: true, validator: vPassword, trigger: 'change'}],
+            };
+            // 登录按钮对密码的验证
+            const vPassword4Ok = function () {
+                const password = loginUser.value.password;
+                const regexpDef = /^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,16}$/;
+                if (password === '') {
+                    return false;
+                }
+                if (!password.match(regexpDef)) {
+                    return false;
+                }
+                return true;
+            };
             // 登录
             const handleLogin = () => {
                 console.log("开始登录");
-                if (Tool.isNotEmpty(loginUser.value.loginName) && Tool.isNotEmpty(loginUser.value.password)) {
-                    loginModalLoading.value = true;
-                    const loginUserReq = Tool.copy(loginUser.value);  // 多复制一个变量，以免登陆错误时会显示转换后的密码
-                    loginUserReq.password = hexMd5(loginUserReq.password + KEY);
-                    axios.post('/user/login', loginUserReq).then((response) => {
-                        const data = response.data;
-                        loginModalLoading.value = false;
-                        if (data.success) {
-                            loginModalVisible.value = false;
-                            store.commit("setUser", data.content);  // 触发函数
-                            message.success("登录成功！");
-                        } else {
-                            message.error(data.message);
-                            loginUser.value.password = '';
-                        }
-                    });
-                }
-                if (Tool.isEmpty(loginUser.value.loginName)) {
-                    message.error('【用户名】不能为空');
-                }
-                if (Tool.isEmpty(loginUser.value.password)) {
-                    message.error('【密码】不能为空');
-                }
+                loginModalLoading.value = true;
+                const loginUserReq = Tool.copy(loginUser.value);  // 多复制一个变量，以免登陆错误时会显示转换后的密码
+                loginUserReq.password = hexMd5(loginUserReq.password + KEY);
+                axios.post('/user/login', loginUserReq).then((response) => {
+                    const data = response.data;
+                    loginModalLoading.value = false;
+                    if (data.success) {
+                        loginModalVisible.value = false;
+                        store.commit("setUser", data.content);  // 触发函数
+                        message.success("登录成功！");
+                    } else {
+                        message.error(data.message);
+                        loginUser.value.password = '';
+                    }
+                });
             };
 
             const showloginModal = () => {
@@ -140,6 +166,8 @@
                 loginUser,
                 showloginModal,
                 handleLogin,
+                rules,
+                vPassword4Ok,
 
                 user,
 
