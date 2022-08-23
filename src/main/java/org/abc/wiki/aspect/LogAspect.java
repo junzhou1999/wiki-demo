f2048e079f47ca76631585a0e2b5a085d1a97948
junzhou1999 <outtatouch2020.com>
2022-08-23 11:17:07 +0800

    11-7 增加生成日志流水号，方便追踪过滤某一事件

--- src/main/java/org/abc/wiki/aspect/LogAspect.java
+++ src/main/java/org/abc/wiki/aspect/LogAspect.java
@@ -3,17 +3,23 @@ package org.abc.wiki.aspect;
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.support.spring.PropertyPreFilters;
 import org.abc.wiki.util.RequestContext;
+import org.abc.wiki.util.SnowFlake;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.Signature;
-import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.annotation.Resource;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
@@ -23,6 +29,9 @@ import javax.servlet.http.HttpServletRequest;
 public class LogAspect {
 
 	private final static Logger LOG = LoggerFactory.getLogger(LogAspect.class);
+
+	@Resource
+	private SnowFlake snowFlake;
 
 	/**
 	 * 定义一个切点Pointcut，针对controller所有的方法，所有的参数
@@ -38,6 +47,9 @@ public class LogAspect {
 	 */
 	@Before("controllerPointcut()")
 	public void doBefore(JoinPoint joinPoint) throws Throwable {
+
+		// 增加业务流水号
+		MDC.put("LOG_ID", String.valueOf(snowFlake.nextId()));
 
 		// 开始打印请求日志
 		ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
--- src/main/java/org/abc/wiki/job/EbookJob.java
+++ src/main/java/org/abc/wiki/job/EbookJob.java
@@ -1,8 +1,10 @@
 package org.abc.wiki.job;
 
 import org.abc.wiki.service.EbookService;
+import org.abc.wiki.util.SnowFlake;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -16,12 +18,17 @@ public class EbookJob {
 	@Resource
 	private EbookService ebookService;
 
+	@Resource
+	private SnowFlake snowFlake;
+
 	/**
 	 * 每秒的44秒，隔一分钟更新一次电子书信息
 	 * 注：这一分钟错过了，就重新算新的一分钟了，隔16秒（44+16）都已经算另一分钟了
 	 */
 	@Scheduled(cron = "44/60 * * * * ? ")
 	public void cron() {
+		// 定时器不经过aop，业务简单，简单增加业务流水号
+		MDC.put("LOG_ID", String.valueOf(snowFlake.nextId()));
 		LOG.info("开始更新电子书下的文档数据");
 		long start = System.currentTimeMillis();
 		ebookService.updateEbookInfo();
