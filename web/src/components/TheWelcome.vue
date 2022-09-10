6441feebc1f03047eabb6ca71f4dfdd5046a2dcf
junzhou1999 <outtatouch2020.com>
2022-09-10 16:27:59 +0800

    12-6 集成echarts-5.3.3，欢迎组件测试示例

--- web/src/components/TheWelcome.vue
+++ web/src/components/TheWelcome.vue
@@ -84,11 +84,22 @@
         </a-col>
     </a-row>
 
+    <br>
+
+    <a-row>
+        <a-col :span="24">
+            <!-- 为 ECharts 准备一个定义了宽高的 DOM -->
+            <div id="main" style="width:100%; height:300px;"></div>
+        </a-col>
+    </a-row>
+
 </template>
 
 <script lang="ts">
     import {defineComponent, ref, onMounted} from 'vue';
     import axios from "axios";
+
+    declare let echarts: any;
 
     export default defineComponent({
         name: 'the-welcome',
@@ -122,9 +133,35 @@
                 })
             };
 
+            const demo = () => {
+                // 基于准备好的dom，初始化echarts实例
+                var myChart = echarts.init(document.getElementById('main'));
+
+                // 绘制图表
+                myChart.setOption({
+                    title: {
+                        text: 'ECharts 入门示例'
+                    },
+                    tooltip: {},
+                    xAxis: {
+                        data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+                    },
+                    yAxis: {},
+                    series: [
+                        {
+                            name: '销量',
+                            type: 'bar',
+                            data: [5, 20, 36, 10, 10, 20]
+                        }
+                    ]
+                });
+            };
+
             onMounted(() => {
                 getStatistic();
+                demo();
             });
+
             return {
                 statistic
             }
