d5816dcd7b312aa8810caabb919b61ccab0223c9
junzhou1999 <outtatouch2020.com>
2022-09-25 23:18:32 +0800

    Revert "101-1 后端增加静态资源的访问" 取消SpringBoot的静态资源处理 This reverts commit 19ecb1dc

--- src/main/java/org/abc/wiki/config/SpringMvcConfig.java
+++ src/main/java/org/abc/wiki/config/SpringMvcConfig.java
@@ -3,20 +3,15 @@ package org.abc.wiki.config;
 import org.abc.wiki.interceptor.LoginInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.annotation.Resource;
-import java.io.File;
 
 @Configuration
 public class SpringMvcConfig implements WebMvcConfigurer {
 
 	@Resource
 	private LoginInterceptor loginInterceptor;
-
-	private final String IMG_PATH =
-			System.getProperty("user.home") + File.separator + "upload-files" + File.separator + "images";
 
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(loginInterceptor)
@@ -26,17 +21,10 @@ public class SpringMvcConfig implements WebMvcConfigurer {
 						"/user/login",            // 登录接口不拦截
 						"/category/all",          // 分类接口不拦截
 						"/ebook/list",            // 查看电子书不拦截，其他增删改拦截
-						"/doc/upload-files/**",   // 静态资源
 						"/doc/find/**",           // 文档信息
 						"/doc/find-content/**",   // 文档内容
 						"/doc/vote/**",           // 点赞功能
 						"/ebook-snapshot/**"      // 数据展示
 				);
-	}
-
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/doc/upload-files/**")
-				.addResourceLocations("file:" + IMG_PATH);
 	}
 }
